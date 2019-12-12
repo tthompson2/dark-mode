795ee29c3521013b7ca753803b5bc23e36f183a0
Trevor Thompson <Thompson_Trevor@bah.com>
2019-12-12 16:43:21 -0500

    Fixed error for rough custom hooks

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 const useLocalStorage = (key, initialValue) => {
     // key, intialValue are the destructed props we are using for the example
 
-    const[name, setName] = useState(() => {
+    const[storedName, setStoredName] = useState(() => {
 
       const item = window.localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
@@ -12,5 +12,7 @@ const useLocalStorage = (key, initialValue) => {
         setName(value);
         window.localStorage.setItem(key, JSON.stringify(value));
     };
-    return [name, setName];
+    return [storedName, setName];
 };
+
+export default useLocalStorage;
